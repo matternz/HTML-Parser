6f7e2e7b0099d9f9256cbd08bbeec61d9ca4c78a
norrismatt <matthew.norris96@gmail.com>
2018-03-12 20:56:17 +1300

    git commit -m " git commit -m

--- src/HTML_Parser.java
+++ src/HTML_Parser.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class HTML_Parser {
-
+	
+	//https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html
+	//https://docs.oracle.com/javase/7/docs/api/java/util/regex/Matcher.html
+	static Pattern PARAGRAPH = Pattern.compile("\n");
+	static Pattern ITALIC = Pattern.compile("\\*");
+	static Pattern BOLD = Pattern.compile("\\*\\*");
+	static Pattern HEADING_1 = Pattern.compile("##");
+	static Pattern HEADING_2 = Pattern.compile("##");
+	static Pattern NUMBERED_LIST = Pattern.compile("[0-9]");
+	static Pattern BULLETED_LIST = Pattern.compile("\\* ");
+	static Pattern SEPERATOR = Pattern.compile("^---");
+	static Pattern BLOCK_QUOTE = Pattern.compile("> $[A-Za-z][A-Za-z0-9]");
+	
+	
+	
+	
+	public HTML_Parser(Scanner scan){
+		
+	}
+	
+	public void parseScanner(Scanner scan){
+		
+	}
+	
+	public static void main(String [] args){
+		
+	}
+	
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,6 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Parser {
 
+	/**
+	 * reads in file to be parsed
+	 * @param file
+	 */
+	public static Scanner readFile(File file){
+		Scanner scan = null;
+		try {
+			scan = new Scanner(file);
+			scan.close();
+			return scan;
+		} catch (FileNotFoundException e) {
+			System.out.println("file not found");
+		}
+		return null;
+	}
+	
+	public static void parseFile(Scanner scan){
+		if(scan == null){
+			return;
+		}
+		
+		HTML_Parser htmlParser;
+		
+	}
+	
+	
+	
 	public static void main(String [] args) {
 
 	}
